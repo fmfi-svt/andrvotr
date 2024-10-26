ff6ddc0a79a02dda233955fbf026e489f8761f80
Tomi Belan <tomi.belan@gmail.com>
2024-10-26 16:30:26 +0200

    build: generate plugin boilerplate
    Content autogenerated by:
    mvn archetype:generate -DarchetypeGroupId=net.shibboleth.idp.plugin -DarchetypeArtifactId=plugin-archetype -DarchetypeVersion=0.0.1-SNAPSHOT -DgroupId=io.github.fmfi_svt -DartifactId=idp-plugin-andrvotr -Dversion=1.0.0-SNAPSHOT -DpluginBase=andrvotr -Dvendor=ŠVT -Dpackage=io.github.fmfi_svt.andrvotr

---
+++ andrvotr-impl/src/main/java/io/github/fmfi_svt/andrvotr/impl/ExamplePlugin.java
@@ -0,0 +1,27 @@
+package io.github.fmfi_svt.andrvotr.impl;
+
+import java.io.IOException;
+import javax.annotation.Nonnull;
+
+import net.shibboleth.idp.plugin.IdPPlugin;
+import net.shibboleth.idp.plugin.PropertyDrivenIdPPlugin;
+import net.shibboleth.profile.plugin.PluginException;
+
+/**
+ * Plugin description about the webauthn plugin.
+ */
+public class ExamplePlugin extends PropertyDrivenIdPPlugin {
+
+    /**
+     * Constructor.
+     *
+     * @param claz type of plugin
+     * 
+     * @throws IOException if properties can't be loaded
+     * @throws PluginException if another error occurs
+     */
+    public ExamplePlugin(@Nonnull final Class<? extends IdPPlugin> claz) throws IOException, PluginException {
+        super(claz);
+    }
+
+}
